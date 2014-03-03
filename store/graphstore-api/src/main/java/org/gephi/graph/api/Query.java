a7d70a9b6cf3a0ca8650c3f3f4222e8a3d4da3d7
Mathieu Bastian <mathieu.bastian@gmail.com>
2014-03-02 16:58:09 -0800

    Split into two modules:api and imll

---
+++ store/graphstore-api/src/main/java/org/gephi/graph/api/Query.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gephi.graph.api;
+
+/**
+ *
+ * @author mbastian
+ */
+public interface Query {
+}
