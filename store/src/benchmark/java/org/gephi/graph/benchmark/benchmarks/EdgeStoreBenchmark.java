faa526dc93ecd9ac60491e193ab44819339245b8
Mathieu Bastian <mathieu.bastian@gmail.com>
2021-11-27 16:01:52 +0100

    Wrap benchmark code as integration test

---
+++ store/src/benchmark/java/org/gephi/graph/benchmark/benchmarks/EdgeStoreBenchmark.java
@@ -0,0 +1,110 @@
+package org.gephi.graph.benchmark.benchmarks;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.gephi.graph.api.Configuration;
+import org.gephi.graph.api.Edge;
+import org.gephi.graph.api.Node;
+import org.gephi.graph.impl.EdgeStore;
+
+/**
+ *
+ * @author mbastian, niteshbhargv
+ */
+public class EdgeStoreBenchmark {
+
+    private Object object;
+    
+    public Runnable pushEdgeStore(int nodes, double prob) {
+        final Configuration config = new Configuration();
+        config.setEdgeIdType(Integer.class);
+        config.setNodeIdType(Integer.class);
+        final RandomGraph graph = new RandomGraph(nodes, prob, config).generate();
+        final EdgeStore edgeStore = graph.getStore().getEdgeStore();
+        final List<Node> nodeList = graph.getNodes();
+        final List<Edge> edgeList = graph.getEdges();
+        graph.getStore().addAllNodes(nodeList);
+        
+        Runnable runnable = () -> {
+            edgeStore.clear();
+            for(Edge edge : edgeList) {
+                edgeStore.add(edge);
+            }
+        };
+        return runnable;
+    }
+
+    public Runnable iterateEdgeStore(int nodes, double prob) {
+        final Configuration config = new Configuration();
+        config.setEdgeIdType(Integer.class);
+        config.setNodeIdType(Integer.class);
+        final RandomGraph graph = new RandomGraph(nodes, prob, config).generate().commit();
+        final EdgeStore edgeStore = graph.getStore().getEdgeStore();
+        
+        Runnable runnable = () -> {
+            Iterator<Edge> itr = edgeStore.iterator();
+            for (; itr.hasNext();) {
+                object = itr.next();
+            }
+        };
+        return runnable;
+    }
+
+    public Runnable iterateEdgeStoreNeighborsOut(int nodes, double prob) {
+        final Configuration config = new Configuration();
+        config.setEdgeIdType(Integer.class);
+        config.setNodeIdType(Integer.class);
+        final RandomGraph graph = new RandomGraph(nodes, prob, config).generate().commit();
+        final EdgeStore edgeStore = graph.getStore().getEdgeStore();
+        final List<Node> nodeList = graph.getNodes();
+        
+        Runnable runnable = () -> {
+            for (Node node : nodeList) {
+                Iterator<Edge> itr = edgeStore.edgeOutIterator(node);
+                for (; itr.hasNext();) {
+                    object = itr.next();
+                }
+            }
+        };
+        return runnable;
+    }
+
+    public Runnable iterateEdgeStoreNeighborsInOut(int nodes, double prob) {
+        final Configuration config = new Configuration();
+        config.setEdgeIdType(Integer.class);
+        config.setNodeIdType(Integer.class);
+        final RandomGraph graph = new RandomGraph(nodes, prob, config).generate().commit();
+        final EdgeStore edgeStore = graph.getStore().getEdgeStore();
+        final List<Node> nodeList = graph.getNodes();
+        
+        Runnable runnable = () -> {
+            for (Node node : nodeList) {
+                Iterator<Edge> itr = edgeStore.edgeIterator(node);
+                for (; itr.hasNext();) {
+                    object = itr.next();
+                }
+            }
+        };
+        return runnable;
+    }
+
+    public Runnable resetEdgeStore(int nodes, double prob) {
+        final Configuration config = new Configuration();
+        config.setEdgeIdType(Integer.class);
+        config.setNodeIdType(Integer.class);
+        final RandomGraph graph = new RandomGraph(nodes, prob, config).generate().commit();
+        final EdgeStore edgeStore = graph.getStore().getEdgeStore();
+        final List<Edge> edgeList = graph.getEdges();
+        
+        Runnable runnable = () -> {
+            for (Edge e : edgeList) {
+                edgeStore.remove(e);
+            }
+            for (Edge e : edgeList) {
+                edgeStore.add(e);
+            }
+        };
+        return runnable;
+    }
+}
---
+++ store/src/benchmark/java/org/gephi/graph/benchmark/benchmarks/LockingBenchmark.java
@@ -0,0 +1,270 @@
+/*
+ * Copyright 2012-2013 Gephi Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.gephi.graph.benchmark.benchmarks;
+
+import java.util.Random;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LockingBenchmark {
+
+    private final DataStruture struture = new DataStruture();
+    private double number;
+    private final int READS = 500;
+    private final int WRITES = 100;
+    private final int READER_THREADS = 4;
+    private final int WRITER_THREADS = 4;
+
+    public Runnable readWithoutLock() {
+        return () -> {
+            for (int i = 0; i < READS; i++) {
+                struture.read();
+            }
+        };
+    }
+
+    public Runnable readWithLock() {
+        final ReadWriteLock lock = new ReentrantReadWriteLock(true);
+        return () -> {
+            for (int i = 0; i < READS; i++) {
+                lock.readLock().lock();
+                struture.read();
+                lock.readLock().unlock();
+            }
+        };
+    }
+
+    public Runnable fairReadOnly() {
+        final ReadWriteLock lock = new ReentrantReadWriteLock(true);
+        return () -> {
+            Runnable r = () -> {
+                for (int i = 0; i < READS; i++) {
+                    lock.readLock().lock();
+                    struture.read();
+                    lock.readLock().unlock();
+                }
+            };
+            Thread[] threads = new Thread[READER_THREADS];
+            for (int i = 0; i < READER_THREADS; i++) {
+                Thread thread = new Thread(r);
+                thread.start();
+                threads[i] = thread;
+            }
+            for (Thread t : threads) {
+                try {
+                    t.join();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(LockingBenchmark.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+    }
+
+    public Runnable unfairReadOnly() {
+        final ReadWriteLock lock = new ReentrantReadWriteLock(false);
+        return () -> {
+            Runnable r = () -> {
+                for (int i = 0; i < READS; i++) {
+                    lock.readLock().lock();
+                    struture.read();
+                    lock.readLock().unlock();
+                }
+            };
+            Thread[] threads = new Thread[READER_THREADS];
+            for (int i = 0; i < READER_THREADS; i++) {
+                Thread thread = new Thread(r);
+                thread.start();
+                threads[i] = thread;
+            }
+            for (Thread t : threads) {
+                try {
+                    t.join();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(LockingBenchmark.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+    }
+
+    public Runnable fairWriteOnly() {
+        final ReadWriteLock lock = new ReentrantReadWriteLock(true);
+        return () -> {
+            Runnable r = () -> {
+                for (int i = 0; i < WRITES; i++) {
+                    lock.writeLock().lock();
+                    struture.write();
+                    lock.writeLock().unlock();
+                }
+            };
+            Thread[] threads = new Thread[READER_THREADS];
+            for (int i = 0; i < READER_THREADS; i++) {
+                Thread thread = new Thread(r);
+                thread.start();
+                threads[i] = thread;
+            }
+            for (Thread t : threads) {
+                try {
+                    t.join();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(LockingBenchmark.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+    }
+
+    public Runnable unfairWriteOnly() {
+        final ReadWriteLock lock = new ReentrantReadWriteLock(false);
+        return () -> {
+            Runnable r = () -> {
+                for (int i = 0; i < WRITES; i++) {
+                    lock.writeLock().lock();
+                    struture.write();
+                    lock.writeLock().unlock();
+                }
+            };
+            Thread[] threads = new Thread[READER_THREADS];
+            for (int i = 0; i < READER_THREADS; i++) {
+                Thread thread = new Thread(r);
+                thread.start();
+                threads[i] = thread;
+            }
+            for (Thread t : threads) {
+                try {
+                    t.join();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(LockingBenchmark.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+    }
+
+    public Runnable fairReadWrites() {
+        final ReadWriteLock lock = new ReentrantReadWriteLock(true);
+        return () -> {
+            Runnable reader = () -> {
+                for (int i = 0; i < READS; i++) {
+                    lock.readLock().lock();
+                    struture.read();
+                    lock.readLock().unlock();
+                }
+            };
+            Runnable writer = () -> {
+                for (int i = 0; i < WRITES; i++) {
+                    lock.writeLock().lock();
+                    struture.write();
+                    lock.writeLock().unlock();
+                }
+            };
+            Thread[] threads = new Thread[READER_THREADS + WRITER_THREADS];
+            for (int i = 0; i < READER_THREADS; i++) {
+                Thread thread = new Thread(reader);
+                threads[i] = thread;
+            }
+            for (int i = 0; i < WRITER_THREADS; i++) {
+                Thread thread = new Thread(writer);
+                threads[READER_THREADS + i] = thread;
+            }
+            for (Thread thread : threads) {
+                thread.start();
+            }
+            for (Thread t : threads) {
+                try {
+                    t.join();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(LockingBenchmark.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+    }
+
+    public Runnable unfairReadWrites() {
+        final ReadWriteLock lock = new ReentrantReadWriteLock(false);
+        return () -> {
+            Runnable reader = () -> {
+                for (int i = 0; i < READS; i++) {
+                    lock.readLock().lock();
+                    struture.read();
+                    lock.readLock().unlock();
+                }
+            };
+            Runnable writer = () -> {
+                for (int i = 0; i < WRITES; i++) {
+                    lock.writeLock().lock();
+                    struture.write();
+                    lock.writeLock().unlock();
+                }
+            };
+            Thread[] threads = new Thread[READER_THREADS + WRITER_THREADS];
+            for (int i = 0; i < READER_THREADS; i++) {
+                Thread thread = new Thread(reader);
+                threads[i] = thread;
+            }
+            for (int i = 0; i < WRITER_THREADS; i++) {
+                Thread thread = new Thread(writer);
+                threads[READER_THREADS + i] = thread;
+            }
+            for (Thread thread : threads) {
+                thread.start();
+            }
+            for (Thread t : threads) {
+                try {
+                    t.join();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(LockingBenchmark.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+    }
+
+    private class DataStruture {
+
+        private final int[] values = new int[10000];
+        private final int readLoops = 10;
+        private final int writeLoops = 5;
+
+        public DataStruture() {
+            Random rand = new Random(454);
+            for (int i = 0; i < values.length; i++) {
+                values[i] = rand.nextInt(values.length);
+            }
+        }
+
+        public void write() {
+            Random rand = new Random(45445);
+            for (int i = 0; i < writeLoops; i++) {
+                for (int j = 0; j < values.length; j++) {
+                    values[j] = rand.nextInt(values.length);
+                }
+            }
+        }
+
+        public void read() {
+            double avg = 0;
+            for (int i = 0; i < readLoops; i++) {
+                double sum = 0;
+                for (int j = 0; j < values.length; j++) {
+                    sum += values[j];
+                }
+                sum /= values.length;
+                avg += sum;
+            }
+            avg /= readLoops;
+            number = avg;
+        }
+    }
+}
